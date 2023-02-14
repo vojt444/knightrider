31b5796e8f5cbeb1fa25aae488a5de46aa0f4498
vojt444 <33757998+vojt444@users.noreply.github.com>
2023-02-14 14:06:24 +0100

    Update macros.h

--- knightrider/macros.h
+++ knightrider/macros.h
@@ -1,8 +1,8 @@
 #ifndef MACROS_H_
 #define MACROS_H_
 
-#define HighBit(adress, bit) (adress |= (1 << bit)) //make output
-#define LowBit(adress, bit) (adress &= ~(1 << bit)) //make input
+#define HighBit(adress, bit) (adress |= (1 << bit))
+#define LowBit(adress, bit) (adress &= ~(1 << bit))
 #define GetBit(adress, bit) (adress & (1 << bit))
 #define ChangeBit(adress, bit) (adress ^ (1 << bit))
 
